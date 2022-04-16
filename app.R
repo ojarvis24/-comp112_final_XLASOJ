3a932261d88d620e911becfc64793c12e526d821
ojarvis24 <ojarvis@macalester.edu>
2022-04-16 12:18:47 -0500

    plot comparing the weather types finished

--- app.R
+++ app.R
@@ -2,6 +2,7 @@ library(shiny)
 library(tidyverse)
 library(maps)
 library(ggmap) 
+library(ggthemes)
 library(lubridate)
 library(sf)  
 library(RColorBrewer)
@@ -30,7 +31,16 @@ ui <- fluidPage(h2("Precipitation in the United States"),
                 selectInput(inputId = "year", 
                             label = "Year:", 
                             choices = c("2016", "2017", "2018", "2019", "2020", "2021")), 
-                plotOutput(outputId = "precipplot"))#widgets
+                plotOutput(outputId = "precipplot"), 
+                h2("What Types of Weather Do Different States Have?"),
+                selectInput(inputId = "state", 
+                            label = "State(s):", 
+                            choices = unique(weather2$state), 
+                            multiple = TRUE), 
+                selectInput(inputId = "year", 
+                            label = "Year:", 
+                            choices = c("2016", "2017", "2018", "2019", "2020", "2021")), 
+                plotOutput(outputId = "typeplot"))#widgets
 
 server <- function(input, output) {
   output$precipplot <- renderPlot(
@@ -45,8 +55,19 @@ server <- function(input, output) {
       labs(fill = "Total Precipitation (inches)") +
       theme_map() +
       theme(legend.background = element_blank()) +
-      scale_fill_distiller(palette = "Blues", direction = 1)
-  )
+      scale_fill_distiller(palette = "Blues", direction = 1))
+  output$typeplot <- renderPlot(
+    weather2 %>% 
+      filter(year == input$year) %>% 
+      filter(state %in% input$state) %>%
+      group_by(state) %>% 
+      group_by(year) %>% 
+      ggplot() +
+      geom_bar(aes(x = Type,
+                   fill = state),
+               position = position_dodge())+
+      labs(fill = "State(s)"))
+    
 } #r code, generate graph, translate input into an output (graph)
 shinyApp(ui = ui, server = server) 
 
